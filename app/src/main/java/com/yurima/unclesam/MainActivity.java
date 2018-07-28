7265aedb1c6272504ea4f4c8eb274e331a86fabd
Yury <31337@mail.ru>
2018-07-28 10:37:55 +0400

    added the second activity

--- app/src/main/java/com/yurima/unclesam/MainActivity.java
+++ app/src/main/java/com/yurima/unclesam/MainActivity.java
@@ -1,13 +1,25 @@
 package com.yurima.unclesam;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button fillButton = (Button) findViewById(R.id.fill_button);
+        fillButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, FillActivity.class);
+        startActivity(intent);
     }
 }
