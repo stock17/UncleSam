52e115a7ee594010d63f4f4d75716b5e9ec039d9
Yury <31337@mail.ru>
2018-08-05 10:44:25 +0400

    added braga activity

--- app/src/main/java/com/yurima/unclesam/MainActivity.java
+++ app/src/main/java/com/yurima/unclesam/MainActivity.java
@@ -4,22 +4,37 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.waterdown_button)
+    ImageButton waterdownButton;
+
+    @BindView(R.id.braga_button)
+    ImageButton bragaButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ImageButton fillButton = (ImageButton) findViewById(R.id.fill_button);
-        fillButton.setOnClickListener(this);
+        ButterKnife.bind(this);
     }
 
-    @Override
-    public void onClick(View v) {
+    @OnClick (R.id.waterdown_button)
+    public void onWaterDownClick(View v) {
         Intent intent = new Intent(this, WaterDownActivity.class);
+        startActivity(intent);
+    }
+
+    @OnClick (R.id.braga_button)
+    public void onBragaClick(View v) {
+        Intent intent = new Intent(this, BragaActivity.class);
         startActivity(intent);
     }
 }
