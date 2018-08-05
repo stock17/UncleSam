b156321947072d864f14cbd2548aba7bb4819399
Yury <31337@mail.ru>
2018-08-05 12:01:22 +0400

    working on braga calculate handler

--- app/src/main/java/com/yurima/unclesam/BragaActivity.java
+++ app/src/main/java/com/yurima/unclesam/BragaActivity.java
@@ -2,12 +2,52 @@ package com.yurima.unclesam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class BragaActivity extends AppCompatActivity {
+
+    @BindView(R.id.braga_waterEditText)
+    EditText waterEditText;
+
+    @BindView(R.id.braga_sugarEditText)
+    EditText sugarEditText;
+
+    @BindView(R.id.braga_gmcheckbox)
+    CheckBox gmCheckBox;
+
+    @BindView(R.id.braga_gmSpinner)
+    Spinner spinner;
+
+    @BindView(R.id.braga_volumeEditText)
+    EditText volumeEditText;
+
+    @BindView(R.id.braga_amountEditText)
+    EditText amountEditText;
+
+    @BindView(R.id.braga_calculateButton)
+    Button calculateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_braga);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick (R.id.braga_calculateButton)
+    public void OnCalculateClick() {
+        String waterText = waterEditText.getText().toString();
+        String sugarText = sugarEditText.getText().toString();
+        String volumeText = volumeEditText.getText().toString();
+        String amountText = amountEditText.getText().toString();
+
+
     }
 }
